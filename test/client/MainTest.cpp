a71097ff3ab0ad3758f296c875a55783788c809a
Piotr Janczura <piotr@janczua.pl>
2019-04-21 15:09:12 +0200

    ref

--- test/client/MainTest.cpp
+++ test/client/MainTest.cpp
@@ -3,15 +3,10 @@
 //
 
 #include "MainTest.h"
-#include "Main.h"
 
 void MainTest::SetUp() {}
 void MainTest::TearDown() {}
 
 
 TEST_F(MainTest, mainTest) {
-    Main m;
-
-    EXPECT_EQ(m.add(1,1) ,2);
-    EXPECT_EQ(m.add(1,1) ,3);
 }
